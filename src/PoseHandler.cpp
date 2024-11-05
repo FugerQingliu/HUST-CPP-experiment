b8bc7bac9557bff52411897998b1f9c68796dd95
FugerQingliu <lrp20050201@qq.com>
2024-11-06 03:15:07 +0800

    extract Point&Direction, simplify the code cyclomatic complexity in PoseHandler through state changes

--- src/PoseHandler.cpp
+++ src/PoseHandler.cpp
@@ -2,68 +2,69 @@
 
 namespace adas
 {
-    PoseHandler::PoseHandler(const Pose &pose) noexcept
-    {
-        this->pose = pose;
-    }
+    PoseHandler::PoseHandler(const Pose &pose) noexcept : point(pose.x,pose.y),facing(&Direction::GetDirection(pose.heading))
+    {}
     void PoseHandler::Move() noexcept
     {
-        {
-            if (pose.heading == 'E')
-            {
-                ++pose.x;
-            }
-            else if (pose.heading == 'W')
-            {
-                --pose.x;
-            }
-            else if (pose.heading == 'N')
-            {
-                ++pose.y;
-            }
-            else if (pose.heading == 'S')
-            {
-                --pose.y;
-            }
-        }
+        point += facing->Move();
+        // {
+        //     if (pose.heading == 'E')
+        //     {
+        //         ++pose.x;
+        //     }
+        //     else if (pose.heading == 'W')
+        //     {
+        //         --pose.x;
+        //     }
+        //     else if (pose.heading == 'N')
+        //     {
+        //         ++pose.y;
+        //     }
+        //     else if (pose.heading == 'S')
+        //     {
+        //         --pose.y;
+        //     }
+        // }
     }
     void PoseHandler::TurnLeft() noexcept
     {
-        if (pose.heading == 'E')
-        {
-            pose.heading = 'N';
-        }
-        else if (pose.heading == 'W')
-        {
-            pose.heading = 'S';
-        }
-        else if (pose.heading == 'N')
-        {
-            pose.heading = 'W';
-        }
-        else if (pose.heading == 'S')
-        {
-            pose.heading = 'E';
-        }
+        facing = &(facing->LeftOne());
+        // if (pose.heading == 'E')
+        // {
+        //     pose.heading = 'N';
+        // }
+        // else if (pose.heading == 'W')
+        // {
+        //     pose.heading = 'S';
+        // }
+        // else if (pose.heading == 'N')
+        // {
+        //     pose.heading = 'W';
+        // }
+        // else if (pose.heading == 'S')
+        // {
+        //     pose.heading = 'E';
+        // }
     }
     void PoseHandler::TurnRight() noexcept
     {
-        if (pose.heading == 'E')
-        {
-            pose.heading = 'S';
-        }
-        else if (pose.heading == 'W')
-        {
-            pose.heading = 'N';
-        }
-        else if (pose.heading == 'N')
-        {
-            pose.heading = 'E';
-        }
-        else if (pose.heading == 'S')
-        {
-            pose.heading = 'W';
-        }
+        facing = &(facing->RightOne());
+        // if (pose.heading == 'E')
+        // {
+        //     pose.heading = 'S';
+        // }
+        // else if (pose.heading == 'W')
+        // {
+        //     pose.heading = 'N';
+        // }
+        // else if (pose.heading == 'N')
+        // {
+        //     pose.heading = 'E';
+        // }
+        // else if (pose.heading == 'S')
+        // {
+        //     pose.heading = 'W';
+        // }
     }
     void PoseHandler::Fast() noexcept
     {
@@ -75,6 +76,6 @@ namespace adas
     }
     Pose PoseHandler::Query(void) const noexcept
     {
-        return pose;
+        return {point.GetX(),point.GetY(),facing->GetHeading()};
     }
 }
