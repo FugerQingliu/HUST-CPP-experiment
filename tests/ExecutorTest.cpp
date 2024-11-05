df58273ab997fc7fd865f9a93826ad87c419790b
FugerQingliu <lrp20050201@qq.com>
2024-11-05 15:19:08 +0800

    feat: Add four test cases for turn left command

--- tests/ExecutorTest.cpp
+++ tests/ExecutorTest.cpp
@@ -34,7 +34,7 @@ namespace adas
         ASSERT_EQ(target, executor->Query());
     }
     // Move Commands
-    //  TestExample1
+    // TestExample 1
     TEST(ExecutorTest, should_return_x_plus_1_given_command_is_M_and_facing_is_E)
     {
         std::unique_ptr<Executor> executor(Executor::NewExecutor({0, 0, 'E'}));
@@ -42,7 +42,7 @@ namespace adas
         const Pose target({1, 0, 'E'});
         ASSERT_EQ(target, executor->Query());
     }
-    // TestExample2
+    // TestExample 2
     TEST(ExecutorTest, should_return_x_minus_1_given_command_is_M_and_facing_is_W)
     {
         std::unique_ptr<Executor> executor(Executor::NewExecutor({0, 0, 'W'}));
@@ -50,7 +50,7 @@ namespace adas
         const Pose target({-1, 0, 'W'});
         ASSERT_EQ(target, executor->Query());
     }
-    // TestExample3
+    // TestExample 3
     TEST(ExecutorTest, should_return_y_plus_1_given_command_is_M_and_facing_is_N)
     {
         std::unique_ptr<Executor> executor(Executor::NewExecutor({0, 0, 'N'}));
@@ -58,12 +58,45 @@ namespace adas
         const Pose target({0, 1, 'N'});
         ASSERT_EQ(target, executor->Query());
     }
-    // TestExample4
+    // TestExample 4
     TEST(ExecutorTest, should_return_y_minus_1_given_command_is_M_and_facing_is_S)
     {
         std::unique_ptr<Executor> executor(Executor::NewExecutor({0, 0, 'S'}));
         executor->Execute("M");
         const Pose target({0, -1, 'S'});
+        ASSERT_EQ(target, executor->Query());
+    }
+    // Turn Left Commands
+    // TestExample 1
+    TEST(ExecutorTest, should_return_facing_N_given_command_is_L_and_facing_is_E)
+    {
+        std::unique_ptr<Executor> executor(Executor::NewExecutor({0, 0, 'E'}));
+        executor->Execute("L");
+        const Pose target({0, 0, 'N'});
+        ASSERT_EQ(target, executor->Query());
+    }
+    // TestExample 2
+    TEST(ExecutorTest, should_return_facing_W_given_command_is_L_and_facing_is_N)
+    {
+        std::unique_ptr<Executor> executor(Executor::NewExecutor({0, 0, 'N'}));
+        executor->Execute("L");
+        const Pose target({0, 0, 'W'});
+        ASSERT_EQ(target, executor->Query());
+    }
+    // TestExample 3
+    TEST(ExecutorTest, should_return_facing_S_given_command_is_L_and_facing_is_W)
+    {
+        std::unique_ptr<Executor> executor(Executor::NewExecutor({0, 0, 'W'}));
+        executor->Execute("L");
+        const Pose target({0, 0, 'S'});
+        ASSERT_EQ(target, executor->Query());
+    }
+    // TestExample 4
+    TEST(ExecutorTest, should_return_facing_E_given_command_is_L_and_facing_is_S)
+    {
+        std::unique_ptr<Executor> executor(Executor::NewExecutor({0, 0, 'S'}));
+        executor->Execute("L");
+        const Pose target({0, 0, 'E'});
         ASSERT_EQ(target, executor->Query());
     }
 }
